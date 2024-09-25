23326415761dbc289f189804b1eb5b7d374b9a2f
luis <luis.melendez.rocha@gmail.com>
2024-09-25 13:36:01 -0600

    PRUEBA CONEXION

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const express = require('express');
 const http = require('http');
 const socketIo = require('socket.io');
 const path = require('path');
-const setupSocket = require('./controllers/socketController');
+// const setupSocket = require('./controllers/socketController');
 
 
 const app = express();
